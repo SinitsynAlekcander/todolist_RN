daaa4643438627f5581ff5e90a8555c4914f187e
SinitsynAlekcander <sinitsyn.alekcander@gmail.com>
2022-09-06 11:39:23 +0300

    Add <Header/>

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.text}>Список дел</Text>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        paddingTop: 60,
+        height: 100,
+        backgroundColor: 'gray',
+    },
+    text: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    }
+});
